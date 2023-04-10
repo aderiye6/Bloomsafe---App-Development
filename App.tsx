28ae70db33bec974f38ed5f9dccccd4006f51f2a
Afeez G Lawal <afeezlg@gmail.com>
2023-04-10 13:19:11 +0100

    chore: add react-query

--- App.tsx
+++ App.tsx
@@ -1,9 +1,17 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {
+  QueryClient,
+  QueryClientProvider,
+  focusManager,
+} from '@tanstack/react-query';
 import * as SplashScreen from 'expo-splash-screen';
 import React from 'react';
+import { AppStateStatus, Platform } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { useAppState } from './hooks/useAppState';
+import { useOnlineManager } from './hooks/useOnlineManager';
 import HomeScreen from './screens/HomeScreen';
 import OnboardingScreen from './screens/OnboardingScreen';
 import SnapChipScreen from './screens/SnapChipScreen';
@@ -12,11 +20,25 @@ import { RootStackParamList } from './types';
 // Keep the splash screen visible while we fetch resources
 SplashScreen.preventAutoHideAsync();
 
+const queryClient = new QueryClient({
+  defaultOptions: { queries: { retry: 2 } },
+});
+
+function onAppStateChange(status: AppStateStatus) {
+  // React Query already supports in web browser refetch on window focus by default
+  if (Platform.OS !== 'web') {
+    focusManager.setFocused(status === 'active');
+  }
+}
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 const App: React.FC = () => {
   const [appIsReady, setAppIsReady] = React.useState(false);
   const [firstLaunch, setFirstLaunch] = React.useState(false);
+
+  useOnlineManager();
+
+  useAppState(onAppStateChange);
 
   React.useEffect(() => {
     async function prepare() {
@@ -57,20 +79,22 @@ const App: React.FC = () => {
   }
 
   return (
-    <SafeAreaProvider>
-      <NavigationContainer>
-        <Stack.Navigator
-          initialRouteName={firstLaunch ? 'Onboarding' : 'Home'}
-          screenOptions={{ headerShown: false }}
-        >
-          {firstLaunch ? (
-            <Stack.Screen name='Onboarding' component={OnboardingScreen} />
-          ) : null}
-          <Stack.Screen name='Home' component={HomeScreen} />
-          <Stack.Screen name='SnapChip' component={SnapChipScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </SafeAreaProvider>
+    <QueryClientProvider client={queryClient}>
+      <SafeAreaProvider>
+        <NavigationContainer>
+          <Stack.Navigator
+            initialRouteName={firstLaunch ? 'Onboarding' : 'Home'}
+            screenOptions={{ headerShown: false }}
+          >
+            {firstLaunch ? (
+              <Stack.Screen name='Onboarding' component={OnboardingScreen} />
+            ) : null}
+            <Stack.Screen name='Home' component={HomeScreen} />
+            <Stack.Screen name='SnapChip' component={SnapChipScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </SafeAreaProvider>
+    </QueryClientProvider>
   );
 };
 
