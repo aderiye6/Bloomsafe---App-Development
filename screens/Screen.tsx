7275ac82c8b54e3c2842ba06aac59243bb6efd82
Afeez G Lawal <afeezlg@gmail.com>
2023-05-09 13:06:39 +0100

    feat: add back button

--- screens/Screen.tsx
+++ screens/Screen.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
-import { View } from 'react-native';
+import { Image, TouchableOpacity, View } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 interface Props {
   children: React.ReactNode;
+  back?: () => void;
 }
 
-const Screen: React.FC<Props> = ({ children }) => {
+const arrowLeftIcon = require('../assets/images/arrowLeft.png');
+
+const Screen: React.FC<Props> = ({ children, back }) => {
   const insets = useSafeAreaInsets();
   return (
     <View
@@ -16,6 +19,25 @@ const Screen: React.FC<Props> = ({ children }) => {
         flex: 1,
       }}
     >
+      {back ? (
+        <TouchableOpacity
+          style={{
+            width: 30,
+            height: 30,
+            borderRadius: 4,
+            marginLeft: 20,
+          }}
+          onPress={back}
+        >
+          <Image
+            source={arrowLeftIcon}
+            style={{
+              width: 30,
+              height: 30,
+            }}
+          />
+        </TouchableOpacity>
+      ) : null}
       {children}
     </View>
   );
