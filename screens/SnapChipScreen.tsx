370db6eb11f79632174c1efa7a59301bc4c96b39
Afeez G Lawal <afeezlg@gmail.com>
2023-05-09 13:07:09 +0100

    feat: make snap chip auth protected

--- screens/SnapChipScreen.tsx
+++ screens/SnapChipScreen.tsx
@@ -1,11 +1,21 @@
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import React from 'react';
 import SnapChip from '../components/SnapChip/SnapChip';
+import { RootStackParamList } from '../types';
+import ProtectedScreen from './ProtectedScreen';
 import Screen from './Screen';
 
-const SnapChipScreen: React.FC = () => {
+type Props = NativeStackScreenProps<RootStackParamList, 'SnapChip'>;
+
+const SnapChipScreen: React.FC<Props> = ({ navigation }) => {
+  const openHistory = () => {
+    navigation.navigate('History');
+  };
   return (
     <Screen>
-      <SnapChip />
+      <ProtectedScreen>
+        <SnapChip openHistory={openHistory} />
+      </ProtectedScreen>
     </Screen>
   );
 };
