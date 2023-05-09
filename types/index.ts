3008dce1b98d0d2a843c98e57d711af67e9ee596
Afeez G Lawal <afeezlg@gmail.com>
2023-05-09 13:03:18 +0100

    chore: add user type

--- types/index.ts
+++ types/index.ts
@@ -9,3 +9,20 @@ export type RootStackParamList = {
   ForgotPassword: undefined;
   ResetPassword: undefined;
 };
+
+export const mockUser = {
+  id: '4',
+  username: null,
+  email: 'com.arrikk@gmail.com',
+  fullname: 'Bruiz Flash',
+  first_name: null,
+  last_name: null,
+  is_verified: 'no',
+  is_active: 'no',
+  status: '1',
+  created_at: '2023-04-06 21:50:30',
+  updated_at: '2023-04-06 21:50:30',
+  token: 'VFpybnJHQ0tCczFFdXQ0ZGcyTWM1N25kLzlMUURWbmF2STJ6a1pZNGxLcz0=',
+};
+
+export type User = typeof mockUser;
