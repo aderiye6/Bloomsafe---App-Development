3091f7edc0a8251acab1f7981b28df95c52043ec
Afeez G Lawal <afeezlg@gmail.com>
2023-05-09 11:56:18 +0100

    chore: update stack param list

--- types/index.ts
+++ types/index.ts
@@ -2,4 +2,10 @@ export type RootStackParamList = {
   Home: undefined;
   Onboarding: undefined;
   SnapChip: undefined;
+  History: undefined;
+  Profile: undefined;
+  Register: undefined;
+  Login: undefined;
+  ForgotPassword: undefined;
+  ResetPassword: undefined;
 };
